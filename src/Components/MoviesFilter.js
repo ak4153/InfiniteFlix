6df9cc8c8ce0269c4a9ee3d63d8e7801c820e018
Alex K <doodlez4k@gmail.com>
2022-01-01 16:15:37 +0200

    main

--- src/Components/MoviesFilter.js
+++ src/Components/MoviesFilter.js
@@ -54,56 +54,113 @@ export default function MoviesFilter({ onFilterSubmit }) {
 }
 
 const MoviesFilterWrapper = styled.div`
-  h4 {
-    margin: 0;
+  @media (min-width: 900px) {
+    h4 {
+      margin: 0;
+    }
+    .moviefilter__body {
+      display: flex;
+      justify-content: space-between;
+      padding: 10px;
+      margin: 20px;
+      border-bottom: 2px solid #cfe0e8;
+      border-top: 2px solid #cfe0e8;
+    }
+    .moviefilter__filters {
+      display: flex;
+      justify-content: space-evenly;
+      flex-grow: 1;
+    }
+    .moviefilter__containers {
+      background-color: #cfe0e8;
+      padding: 0;
+      border-radius: 10px;
+    }
+    .moviefilter__year {
+      border: none;
+      background: transparent;
+      padding: 0;
+      height: 100%;
+      outline: none;
+      -moz-appearance: textfield;
+      -webkit-appearance: none;
+    }
+    .moviefilter__year::-webkit-outer-spin-button,
+    .moviefilter__year::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    .moviefilter__rating {
+      border: none;
+      outline: none;
+      background-color: transparent;
+    }
+    .moviefilter__submitButton {
+      color: #ffffff;
+      background-color: #cfe0e8;
+      font-size: 19px;
+      border: 1px solid #2d63c8;
+      padding: 10;
+      cursor: pointer;
+      color: black;
+      &:hover {
+        background-color: #65a2bf;
+      }
+    }
   }
-  .moviefilter__body {
-    display: flex;
-    justify-content: space-between;
-    padding: 10px;
-    margin: 20px;
-    border-bottom: 2px solid #cfe0e8;
-    border-top: 2px solid #cfe0e8;
-  }
-  .moviefilter__filters {
-    display: flex;
-    justify-content: space-evenly;
-    flex-grow: 1;
-  }
-  .moviefilter__containers {
-    background-color: #cfe0e8;
-    padding: 0;
-    border-radius: 10px;
-  }
-  .moviefilter__year {
-    border: none;
-    background: transparent;
-    padding: 0;
-    height: 100%;
-    outline: none;
-    -moz-appearance: textfield;
-    -webkit-appearance: none;
-  }
-  .moviefilter__year::-webkit-outer-spin-button,
-  .moviefilter__year::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-    margin: 0;
-  }
-  .moviefilter__rating {
-    border: none;
-    outline: none;
-    background-color: transparent;
-  }
-  .moviefilter__submitButton {
-    color: #ffffff;
-    background-color: #cfe0e8;
-    font-size: 19px;
-    border: 1px solid #2d63c8;
-    padding: 10;
-    cursor: pointer;
-    color: black;
-    &:hover {
-      background-color: #65a2bf;
+  @media (min-width: 320px) {
+    h4 {
+      margin: 0;
+    }
+    .moviefilter__body {
+      display: flex;
+      justify-content: space-between;
+      padding: 5px;
+      margin: 10px;
+      border-bottom: 2px solid #cfe0e8;
+      border-top: 2px solid #cfe0e8;
+      flex-direction: column;
+    }
+    .moviefilter__filters {
+      display: flex;
+      justify-content: space-evenly;
+      flex-grow: 1;
+    }
+    .moviefilter__containers {
+      background-color: #cfe0e8;
+      padding: 0;
+      border-radius: 10px;
+    }
+    .moviefilter__year {
+      border: none;
+      background: transparent;
+      padding: 0;
+      height: 100%;
+      outline: none;
+      -moz-appearance: textfield;
+      -webkit-appearance: none;
+    }
+    .moviefilter__year::-webkit-outer-spin-button,
+    .moviefilter__year::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+    .moviefilter__rating {
+      border: none;
+      outline: none;
+      background-color: transparent;
+    }
+    .moviefilter__submitButton {
+      color: #ffffff;
+      background-color: #cfe0e8;
+      font-size: 19px;
+      border: 1px solid #2d63c8;
+      padding: 10;
+      cursor: pointer;
+      color: black;
+      &:hover {
+        background-color: #65a2bf;
+      }
     }
   }
 `;
